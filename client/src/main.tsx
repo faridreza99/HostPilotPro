ffbd73208d33155fae4a39e7717b5534dd04b296
maxtechctg <maxtechctg@gmail.com>
2025-11-13 14:40:57 +0600

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -29,9 +29,4 @@ document.addEventListener("DOMContentLoaded", () => {
   }, 3000); // Wait longer for React to render and attach handlers
 });
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+createRoot(document.getElementById("root")!).render(<App />);
